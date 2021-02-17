a6464735ecb68d6abf8cc3fed92db7c3f2f76b2f
jnutterdev <hello@jnutterdev.com>
2021-02-16 22:08:26 -0500

    updated readme file with current stack

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -4,34 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1 className="title">Bulma</h1>
-<p className="subtitle">
-  Modern CSS framework based on{' '}
-  <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
-    Flexbox
-  </a>
-</p>
-
-<div className="field">
-  <div className="control">
-    <input className="input" type="text" placeholder="Input" />
-  </div>
-</div>
-
-<div className="field">
-  <p className="control">
-    <span className="select">
-      <select>
-        <option>Select dropdown</option>
-      </select>
-    </span>
-  </p>
-</div>
-
-<div className="buttons">
-  <a className="button is-primary">Primary</a>
-  <a className="button is-link">Link</a>
-</div>
+     <h1 className="title">Coming soon!</h1>
     </div>
   );
 }
