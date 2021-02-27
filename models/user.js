8fedd142ce83be4988e61e00a63d5579a33d7560
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2021-02-27 12:01:24 -0500

    linking user table with mytrips table

--- models/user.js
+++ models/user.js
@@ -10,14 +10,16 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.hasMany(models.MyTrips, {
+        foreignKey: 'userId'
+      });
     }
   };
   User.init({
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
-    myTripId: DataTypes.STRING,
+    myTripId: DataTypes.INTEGER,
     travelDates: DataTypes.STRING
   }, {
     sequelize,
