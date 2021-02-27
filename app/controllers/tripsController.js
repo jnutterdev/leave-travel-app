bb1c46dfee5d690d3b9b962426ccdb79b19a00e6
Tara <taradavies@gmail.com>
2021-02-27 15:03:26 -0500

    pushing up latest code before stepping away. post a trip, get all trips are good to go

--- app/controllers/tripsController.js
+++ app/controllers/tripsController.js
@@ -1,16 +1,16 @@
 // const db = require('../../models');
-const {MyTrips} = require('../../models');
+const { MyTrips, User } = require('../../models');
 // const Trips = db.mytrips;
 
 // CREATES Trip (enters trip details, selects place, saves)
 exports.mytrips_create_post = async function(req, res) {
       
-    const { placeId, place, userID, reservations, travelDates} = req.body;
+    const { placeId, place, userId, reservations, travelDates} = req.body;
     
     const newTrip = await MyTrips.create({
       placeId,
       place,
-      userID,
+      userId,
       reservations,
       travelDates,
      });
@@ -21,12 +21,24 @@ exports.mytrips_create_post = async function(req, res) {
   
 
 // get all trips
-exports.mytrips_findOneUser_get = async function(req, res) {
+exports.mytrips_get = async function (req, res) 
+ {
+    
+    // const oneUser = await MyTrips.findAll(req.params.userId);
+    const oneUser = await MyTrips.findAll({
+        userId: req.param.userId,
+    });
+    res.send(oneUser);
+};
+
+
+/* exports.mytrips_findOneUser_get = async function(req, res) {
 
     const MyTrips = await MyTrips.findAll()
     
-    res.send('/mytrips');
-  };
+    res.send('/mytrips/:userId');
+  }; */
+
 // add new reservation (put for new)
 
 // update trip (patch for update)
