7165504798d5fda31dd1ab365df51b500400a63a
Tara <taradavies@gmail.com>
2021-02-27 20:19:32 -0500

    finalized user controllers and routes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -12,14 +12,6 @@ const users_controller = require('../controllers/usersController');
 // CREATES trip (enters trip details, selects place, saves - tested and works)
 router.post('/mytrips', trips_controller.mytrips_create_post);
 
-/* async (req,res) => {
-    try {
-        console.log(req.body);
-    } catch (error) {
-        console.error(err.message);
-    }
-}); */
-
 // GET all My Trips (tested and works)
 router.get('/mytrips/:userId', trips_controller.mytrips_get);
 
@@ -33,3 +25,14 @@ router.patch('/mytrips/:id', trips_controller.mytrips_update_patch);
 // delete a trip (tested and works)
 router.delete('/mytrips/:id', trips_controller.mytrips_delete_post);
 
+
+// USER ROUTES
+
+//create a user - register (tested and works)
+router.post('/user', users_controller.user_create_post);
+
+//get a user - login (tested and works)
+router.get('/user/:id', users_controller.user_get);
+
+//delete a user - delete acct
+router.delete('/user/:id', users_controller.user_delete_post); 
